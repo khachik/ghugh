9be389c9dd5b5d94fdfe40d002d92a757205d8a3
khachik <khachik.kocharyan@gmail.com>
2012-10-04 10:38:30 +0400

    added installer

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='ghugh',
+      version='0.1',
+      description='Machine Learning Library',
+      author='khachik',
+      url='https://github.com/khachik/ghugh',
+      packages=['ghugh'],
+      license='See LICENSE.txt'
+     )
